ab9195dadab3498cba9a8435262bb114b4f37b74
ThuHienNguy <94470658+ThuHienNguy@users.noreply.github.com>
2021-11-18 18:39:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,21 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    introTitle: 'Hà Xuân Hòa (HX)',
+    introDesc: `Chúc cạu, người tớ yêu sẽ luôn vui vẻ, mạnh khỏe, hạnh phúc
+    Luôn tự tin vững bước trên con đường đã chọn
+    Nếu mệt mỏi, hãy ngoảnh lại phía sau, sẽ có tớ, luôn bên cạu, cùng cậu vượt qua mỏi thử thách, cùng cạu sẻ chia những buồn vui hay đau khổ. Dù có chuyện gì xảy ra, tớ mong mình sẽ là người được biết chuyện để được giúp đỡ cạu, có chuyện gì hãy nói với tớ nhé <3
+    Luv u forever <3`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy:> ',
+    btnYes: 'Thích lắm 💖💖💖',
+    btnNo: '💔 Không nha ! 💔 ',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cạuu lại yêu tớ <3 😊😊',
     reasonPlaceholder: 'Lý do',
-    btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnReply: 'Gửi cho cạuuu',
+    reply: 'Yêu thì yêu không yêu thì yêu 😊😊 <333333',
+    mess: ' Tớ biết mà 🥰. Yêu cạuu nhiều nhiều 😘😘Love you 300.000 😘😘',
+    messDesc: 'Chúng ta sẽ bên nhau mãi mãi nhé <3333',
     btnAccept: 'Ok lun 💖',
     messLink: 'https://m.me/PhungQuocDinh'
 }
